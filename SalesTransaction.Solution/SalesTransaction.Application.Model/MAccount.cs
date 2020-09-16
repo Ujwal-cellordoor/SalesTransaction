78b71eee5ae11744240efd0056f6a0bc0617a8fe
Ujwal <ujalrai9@gmail.com>
2020-09-16 20:30:20 +0545

    initial commit

---
+++ SalesTransaction.Solution/SalesTransaction.Application.Model/MAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SalesTransaction.Application.Model
+{
+    public class MAccount
+    {
+    }
+
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SalesTransaction.Solution/SalesTransaction.Application.Service/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using SalesTransaction.Application.Model.Account;
+
+namespace SalesTransaction.Application.Service.Account
+{
+    public interface IAccountService
+    {
+        dynamic GetLogin(MLogin login);
+        dynamic GetUserDetail(string json);
+    }
+}
